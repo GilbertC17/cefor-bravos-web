7d9a0ec0579b2e5acaba097977d40dfcbb1858a4
Gilberto Garza Castillo <garzacastillogilberto17@gmail.com>
2025-03-18 19:17:30 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import logo from './img/Logo_tehuacan.png';
 import './App.css';
+import './bootstrap/css/bootstrap.min.css';
+import './bootstrap/js/bootstrap.min.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar navbar-expand-lg custom-navbar">
+  <div className="container-fluid center">
+    <img src={logo} className="logo-nav" alt="Logo"></img>
+    <a className="navbar-brand" href="#">CENTRO DE FORMACIÓN <br /> BRAVOS TEHUACÁN</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Equipo</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Noticias</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Brimmer</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Otros Centros</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Iniciar Sesión</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   );
 }
 
